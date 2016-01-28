d6107098a63ebee57101434efde076b6846e3015
Tanner Evins <tanner@tannerevins.com>
2016-01-28 12:56:25 -0600

    Add Validation as an applicative version of Either

--- answers/src/main/scala/fpinscala/practice/applicative/Applicative.scala
+++ answers/src/main/scala/fpinscala/practice/applicative/Applicative.scala
@@ -37,6 +37,24 @@ trait Applicative[F[_]] extends Functor[F] {
     map2(fab,fa)((ab,a) => ab(a))
 }
 
+sealed trait Validation[+E,+A]
+
+case class Failure[E](head: E, tail: Vector[E] = Vector())
+  extends Validation[E,Nothing]
+
+case class Success[A](a: A) extends Validation[Nothing,A]
+
 object Applicative {
+
+  def validationApplicative[E] = new Applicative[({type f[x] = Validation[E,x]})#f] {
+    def apply[A,B](fab: Validation[E,A => B])(fa: Validation[E,A]): Validation[E,B] =
+      (fa,fab) match {
+        case (Success(a),Success(f)) => Success(f(a))
+        case (Failure(h1,t1),Failure(h2,t2)) => Failure(h1,t1++Vector(h2)++t2)
+        case (e@Failure(_,_),_) => e
+        case (_,e@Failure(_,_)) => e
+      }
+    def unit[A](a: => A): Validation[E,A] = Success(a)
+  }
 }
 
